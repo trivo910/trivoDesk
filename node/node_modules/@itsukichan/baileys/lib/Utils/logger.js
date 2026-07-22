5db5cc6e4255fd608cdf8c6d709db8a0db58d26f
vilassontakke <vilassontakke123@gmail.com>
2026-07-22 17:29:14 +0530

    first commit

--- node/node_modules/@itsukichan/baileys/lib/Utils/logger.js
+++
@@ -1,7 +0,0 @@
-"use strict"
-
-Object.defineProperty(exports, "__esModule", { value: true })
-
-const pino = require("pino")
-
-exports.default = pino({ timestamp: () => `,"time":"${new Date().toJSON()}"` })
